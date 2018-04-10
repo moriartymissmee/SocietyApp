af1bf052af25bf7b286ecb2bfff299932bb5f3ff
Jim <moriarty.miss.mee@gmail.com>
2018-04-11 02:10:43 +0530

    First Commit

---
+++ SocietyApp/app/src/main/java/com/example/sundeepsingh/societyapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.sundeepsingh.societyapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    return true;
+                case R.id.navigation_notifications:
+                    selectedFragment = new Querry();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+                    return true;
+            }
+
+
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
